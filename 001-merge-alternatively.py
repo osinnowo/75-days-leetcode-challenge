d6bf29fb5e1d46996e51cf4e2916e5dd6d11dfc4
osinnowo <liquidcoding2009@gmail.com>
2024-01-05 20:05:49 +0100

    001 - Merge Alternatively

---
+++ 001-merge-alternatively.py
@@ -0,0 +1,22 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        result = ""
+        i = 0
+        j = 0
+
+        while i <= len(word1) - 1 and j <= len(word2) - 1:
+            char1 = word1[i]
+            char2 = word2[j]
+            result += (char1 + char2)
+            i += 1
+            j += 1
+        else:
+            while i <= len(word1) - 1:
+                result += word1[i]
+                i += 1
+            
+            while j <= len(word2) - 1:
+                result += word2[j]
+                j += 1
+      
+        return result
