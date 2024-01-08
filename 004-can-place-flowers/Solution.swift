189c4fd38a0a2a1a897212fb4dcf5faab258d408
osinnowo <liquidcoding2009@gmail.com>
2024-01-08 22:34:03 +0100

    004 - Can place flowers

---
+++ 004-can-place-flowers/Solution.swift
@@ -0,0 +1,20 @@
+class Solution {
+    func canPlaceFlowers(_ flowerbed: [Int], _ n: Int) -> Bool {
+        var flowerbed = flowerbed
+        var count = 0 
+
+        for i in 0..<flowerbed.count {
+            if flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) 
+            && (i == flowerbed.count - 1 || flowerbed[i + 1] == 0) {
+                flowerbed[i] = 1
+                count += 1 
+            }
+
+            if count >= n { 
+                return true
+            }
+        }
+
+        return false 
+    }
+}
