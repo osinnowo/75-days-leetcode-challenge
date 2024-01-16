b2bdc2de63d0e0966217a67608d5e34ef12298c1
osinnowo <liquidcoding2009@gmail.com>
2024-01-16 22:56:58 +0100

    Max Operation

---
+++ 013-max-operation/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int max = 0;
+
+        for(var num: nums) {
+            int complement = k - num;
+
+            if(map.containsKey(complement) && map.get(complement) > 0) {
+                max += 1;
+                map.put(complement, map.get(complement) - 1);
+            } else {
+                if(!map.containsKey(num)) {
+                    map.put(num, 0);
+                }
+                map.put(num, map.get(num) + 1);
+            }
+        }
+
+        return max;
+    }
+}
