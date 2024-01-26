de10431fc842ad8a5a2645ad32b8a60710674a3d
osinnowo <liquidcoding2009@gmail.com>
2024-01-26 23:03:14 +0100

    023 - delete a linkedlist

---
+++ 023-delete-linkedlist-node/Solution.py
@@ -0,0 +1,23 @@
+class ListNode:
+    def __init__(self, value=0, next=None):
+        self.value = value
+        self.next = next
+
+class Solution:
+    def deleteMiddle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return None  # No middle node to delete
+        
+        slow = head
+        fast = head
+        prev = None
+
+        while fast and fast.next:
+            prev = slow
+            slow = slow.next
+            fast = fast.next.next
+
+        # Delete the middle node
+        prev.next = slow.next
+
+        return head
